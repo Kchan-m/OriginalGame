1377fd7053b8bd1cfd99d1d619eb4afea9c58e7f
Kchan-m <atr10180108@gmail.com>
2023-09-22 23:02:06 +0900

    BGMの追加

--- Assets/Scripts/Bgm.cs
+++ Assets/Scripts/Bgm.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Bgm : MonoBehaviour
 {
+    public static Bgm instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+
     }
 
     // Update is called once per frame
